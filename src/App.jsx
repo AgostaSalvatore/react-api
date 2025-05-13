ce934245337a88ba1d8873157fae2ee29693a3e6
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-13 13:40:03 +0200

    [UPD] Milestone 1 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
+  const [actress, setActress] = useState([])
+  const endpoint = 'https://lanciweb.github.io/demo/api/actresses'
+
+  useEffect(() => {
+    fetchActress()
+  }, [])
+
+  const fetchActress = () => {
+    axios.get(endpoint)
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(`Error Fetching Actress: ${error}`))
+  }
 
   return (
     <>
